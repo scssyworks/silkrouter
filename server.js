e11bf2ab5df289531b91822be2bedbf628dfc6c5
Sachin Singh 5 <ssingh259@sapient.com>
2017-10-20 21:01:53 +0530

    Made changes in server.js to make it production ready

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ app.set("views", path.join(__dirname, "src"));
 app.set('view engine', 'ejs');
 app.use("/", express.static(path.join(__dirname, "src")));
 app.get("*", function (req, res) {
-    res.render("index", { path: req.url });
+    res.render("index", {
+        path: req.url
+    });
 });
-app.listen(8800, function () {
-    console.log("Node server started at PORT:" + 8800);
+var server = app.listen((process.env.PORT || 8800), function () {
+    console.log("Node server started at PORT:" + server.address().port);
 });
