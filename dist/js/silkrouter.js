c733833eff58ac1dfdb6bfaabf27bc146986c72c
Sachin Singh <ssingh259@sapient.com>
2019-06-22 12:20:46 +0530

    New version generated

--- dist/js/silkrouter.js
+++ dist/js/silkrouter.js
@@ -3,7 +3,7 @@
  * Released under MIT license
  * @name Silk router
  * @author Sachin Singh <ssingh.300889@gmail.com>
- * @version 3.0.1
+ * @version 3.0.2
  * @license MIT
  */
 (function (global, factory) {
