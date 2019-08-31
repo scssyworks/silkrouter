9e26a9ba301c308afa1d86255ce5094b3efc2e14
Sachin Singh <ssingh259@sapient.com>
2019-08-31 15:58:57 +0530

    Added test cases to improve coverage

--- tests/deparam.test.js
+++ tests/deparam.test.js
@@ -9,6 +9,17 @@ describe('Deparam module', () => {
         });
     });
 
+    test('Deparam should convert simple query string to object if query string starts with "?"', () => {
+        expect(deparam('?param1=hello&param2=world')).toEqual({
+            param1: 'hello',
+            param2: 'world'
+        });
+    });
+
+    test('Deparam should return empty object if query string is empty', () => {
+        expect(deparam('')).toEqual({});
+    });
+
     test('Deparam should coerce values if coercion is enabled', () => {
         expect(deparam('param1=10&param2=false')).toEqual({
             param1: 10,
@@ -30,7 +41,46 @@ describe('Deparam module', () => {
         });
     });
 
+    test('Deparam should convert really complex query string to object', () => {
+        expect(deparam('a=10&b=20&c[]=30&d[0]=40&e[f]=60&e[g][]=70&k[i]=hello&k[i][][j]=world')).toEqual({
+            a: 10,
+            b: 20,
+            c: [30],
+            d: [40],
+            e: {
+                f: 60,
+                g: [70]
+            },
+            k: {
+                i: [
+                    [
+                        'hello',
+                        {
+                            j: 'world'
+                        }
+                    ]
+                ]
+            }
+        });
+    });
+
+    test('Deparam should support old array parsing formats', () => {
+        expect(deparam('a=10&a=20')).toEqual({
+            a: [10, 20]
+        });
+    });
+
     test('Deparam should convert the stringified query string back to same object', () => {
         expect(deparam(toQueryString({ p1: 'test', p2: ['Array test'], p3: { cp1: 'object test' } }))).toEqual({ p1: 'test', p2: ['Array test'], p3: { cp1: 'object test' } });
+    });
+
+    test('Deparam should coerce boolean, undefined, null and NaN values properly', () => {
+        expect(deparam('a=null&b=undefined&c=false&d=NaN&e=true')).toEqual({
+            a: null,
+            b: undefined,
+            c: false,
+            d: NaN,
+            e: true
+        });
     });
 });
