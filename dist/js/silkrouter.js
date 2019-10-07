5d87e4c5f0b1848db99d8d493a36e1570881b5a7
Sachin Singh <ssingh259@sapient.com>
2019-10-07 11:28:30 +0530

    Bumped version

--- dist/js/silkrouter.js
+++ dist/js/silkrouter.js
@@ -3,7 +3,7 @@
  * Released under MIT license
  * @name Silk router
  * @author Sachin Singh <contactsachinsingh@gmail.com>
- * @version 3.4.1
+ * @version 3.4.2
  * @license MIT
  */
 (function (global, factory) {
