52081c9fe7376f373a425ae6aaa2cc570a51adc3
Sachin Singh <ssingh259@sapient.com>
2020-01-28 11:22:04 +0530

    Added typings

--- src/typings/silkrouter.d.ts
+++ src/typings/silkrouter.d.ts
@@ -1,34 +1,33 @@
-declare namespace router {
-    namespace api {
-        function trigger(eventName: string, routeConfig: object, originalData: object): void;
-        function extractParams(path: string): object;
-        function toQueryString(query: object): string;
-    }
-    function set(route: Router.RouterOptions, replaceMode: boolean, noTrigger: boolean): void;
+import { Subscription, Observable } from "rxjs";
+
+declare class Router {
+    constructor(config: RouterConstructorOption);
+    config: RouterConstructorOption;
+    subscribe(callback: Function): Subscription;
+    pipe(...args: Function[]): Observable;
+    set(routeConfig: string | RouteConfigOption, replace?: boolean, exec?: boolean): Router;
 }
 
-declare function route(route: string, handler: Function): void;
-
-declare function routeIgnoreCase(route: string, handler: Function): void;
-
-declare function unroute(route: string, handler: Function): void;
-
-declare function deparam(query: string): object;
-
-declare function param(query: object): string;
-
-declare function routeParams(path: string): object;
-
-declare namespace Router {
-    export interface RouterOptions {
-        route: string,
-        data?: any,
-        title?: string,
-        queryString?: string,
-        appendQuery?: boolean,
-        replaceMode?: boolean,
-        noTrigger?: boolean
-    }
+declare interface RouterConstructorOption {
+    hashRouting?: boolean,
+    preservePath?: boolean,
+    context?: Node
 }
 
-export { router, route, unroute, deparam, param, routeParams };
+declare interface RouteConfigOption {
+    route: string,
+    queryString?: string | object,
+    pageTitle?: string,
+    data?: any,
+    preserveQuery?: boolean,
+    replace?: boolean,
+    exec?: boolean
+}
+
+declare namespace operators {
+    function route(route: string, routerInstance?: Router, ignoreCase?: boolean): Function;
+    function deparam(coerce?: boolean): Function;
+    function noMatch(routerInstance?: Router): Function;
+}
+
+export { Router, operators };
