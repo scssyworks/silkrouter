bd8bc6e1bba03dcca3c34e7d24165d3b25bc5616
Sachin Singh <ssingh259@sapient.com>
2020-05-26 15:48:49 +0530

    Updated babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import babel from "rollup-plugin-babel";
+import babel from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
 import { eslint } from 'rollup-plugin-eslint';
 import serve from 'rollup-plugin-serve';
