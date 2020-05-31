ec474d12c1d743cf88611e495fa271cef1151b81
Sachin Singh <ssingh259@sapient.com>
2020-05-31 19:03:54 +0530

    Bumped version

--- dist/esm/index.esm.js
+++ dist/esm/index.esm.js
@@ -3,7 +3,7 @@
  * Released under MIT license
  * @name Silk router
  * @author Sachin Singh <contactsachinsingh@gmail.com>
- * @version 3.5.6
+ * @version 3.5.7
  * @license MIT
  */
 var HASH_CHANGE = 'hashchange';
