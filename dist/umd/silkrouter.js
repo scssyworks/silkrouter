465cc19ecc2aa95b27a749176dbedc349604a1d9
Sachin Singh <ssingh259@sapient.com>
2021-05-11 05:02:35 +0530

    Production build

--- dist/umd/silkrouter.js
+++ dist/umd/silkrouter.js
@@ -144,7 +144,6 @@
   var REG_REPLACE_NEXTPROP = /[^[]+/;
   var HISTORY_UNSUPPORTED = 'Current browser does not support history object';
   var INVALID_ROUTE = 'Route string is not a pure route';
-  var LOCAL_ENV = ['localhost', '0.0.0.0', '127.0.0.1', null];
   var VIRTUAL_PUSHSTATE = 'vpushstate';
   var CACHED_FIELDS = ['route', 'hashRouting', 'path', 'hash', 'search', 'hashSearch', 'data'];
 
@@ -185,15 +184,6 @@
 
   function isValidRoute(route) {
     return typeof route === 'string' && REG_PATHNAME.test(route);
-  }
-  /**
-   * Checks if key is present in provided object
-   * @param {object} ob Object
-   * @param {*} key Key
-   */
-
-  function hasOwn(ob, key) {
-    return Object.prototype.hasOwnProperty.call(ob, key);
   }
   /**
    * Loops over an array like object
@@ -295,396 +285,6 @@
     }
   }
 
-  var loc = window.location;
-  var f = String.fromCharCode;
-
-  function _update(context, bitsPerChar, getCharFromInt) {
-    if (context.context_data_position == bitsPerChar - 1) {
-      context.context_data_position = 0;
-      context.context_data.push(getCharFromInt(context.context_data_val));
-      context.context_data_val = 0;
-    } else {
-      context.context_data_position++;
-    }
-  }
-
-  function _updateContextNumBits(context) {
-    context.context_enlargeIn--;
-
-    if (context.context_enlargeIn == 0) {
-      context.context_enlargeIn = Math.pow(2, context.context_numBits);
-      context.context_numBits++;
-    }
-  }
-
-  function _updateContext(context, bitsPerChar, getCharFromInt) {
-    if (hasOwn(context.context_dictionaryToCreate, context.context_w)) {
-      if (context.context_w.charCodeAt(0) < 256) {
-        for (var i = 0; i < context.context_numBits; i++) {
-          context.context_data_val = context.context_data_val << 1;
-
-          _update(context, bitsPerChar, getCharFromInt);
-        }
-
-        context.value = context.context_w.charCodeAt(0);
-
-        for (var _i = 0; _i < 8; _i++) {
-          context.context_data_val = context.context_data_val << 1 | context.value & 1;
-
-          _update(context, bitsPerChar, getCharFromInt);
-
-          context.value = context.value >> 1;
-        }
-      } else {
-        context.value = 1;
-
-        for (var _i2 = 0; _i2 < context.context_numBits; _i2++) {
-          context.context_data_val = context.context_data_val << 1 | context.value;
-
-          _update(context, bitsPerChar, getCharFromInt);
-
-          context.value = 0;
-        }
-
-        context.value = context.context_w.charCodeAt(0);
-
-        for (var _i3 = 0; _i3 < 16; _i3++) {
-          context.context_data_val = context.context_data_val << 1 | context.value & 1;
-
-          _update(context, bitsPerChar, getCharFromInt);
-
-          context.value = context.value >> 1;
-        }
-      }
-
-      _updateContextNumBits(context);
-
-      delete context.context_dictionaryToCreate[context.context_w];
-    } else {
-      context.value = context.context_dictionary[context.context_w];
-
-      for (var _i4 = 0; _i4 < context.context_numBits; _i4++) {
-        context.context_data_val = context.context_data_val << 1 | context.value & 1;
-
-        _update(context, bitsPerChar, getCharFromInt);
-
-        context.value = context.value >> 1;
-      }
-    }
-
-    _updateContextNumBits(context);
-  }
-
-  function compress(uncompressed, bitsPerChar, getCharFromInt) {
-    if (uncompressed == null) return '';
-    var context = {
-      context_dictionary: {},
-      context_dictionaryToCreate: {},
-      context_data: [],
-      context_c: "",
-      context_wc: "",
-      context_w: "",
-      context_enlargeIn: 2,
-      context_dictSize: 3,
-      context_numBits: 2,
-      context_data_val: 0,
-      context_data_position: 0
-    };
-    var i = 0;
-
-    for (var ii = 0; ii < uncompressed.length; ii += 1) {
-      context.context_c = uncompressed.charAt(ii);
-
-      if (!hasOwn(context.context_dictionary, context.context_c)) {
-        context.context_dictionary[context.context_c] = context.context_dictSize++;
-        context.context_dictionaryToCreate[context.context_c] = true;
-      }
-
-      context.context_wc = context.context_w + context.context_c;
-
-      if (hasOwn(context.context_dictionary, context.context_wc)) {
-        context.context_w = context.context_wc;
-      } else {
-        _updateContext(context, bitsPerChar, getCharFromInt);
-
-        context.context_dictionary[context.context_wc] = context.context_dictSize++;
-        context.context_w = String(context.context_c);
-      }
-    }
-
-    if (context.context_w !== "") {
-      _updateContext(context, bitsPerChar, getCharFromInt);
-    }
-
-    context.value = 2;
-
-    for (i = 0; i < context.context_numBits; i++) {
-      context.context_data_val = context.context_data_val << 1 | context.value & 1;
-
-      _update(context, bitsPerChar, getCharFromInt);
-
-      context.value = context.value >> 1;
-    } // Flush the last char
-    // eslint-disable-next-line
-
-
-    while (true) {
-      context.context_data_val = context.context_data_val << 1;
-
-      if (context.context_data_position == bitsPerChar - 1) {
-        context.context_data.push(getCharFromInt(context.context_data_val));
-        break;
-      } else context.context_data_position++;
-    }
-
-    return context.context_data.join('');
-  }
-
-  function _commonRep3(data, maxpower, resetValue, getNextValue) {
-    var bits = 0;
-    var power = 1;
-
-    while (power !== maxpower) {
-      var resb = data.val & data.position;
-      data.position >>= 1;
-
-      if (data.position === 0) {
-        data.position = resetValue;
-        data.val = getNextValue(data.index++);
-      }
-
-      bits |= (resb > 0 ? 1 : 0) * power;
-      power <<= 1;
-    }
-
-    return bits;
-  }
-
-  function decompress(length, resetValue, getNextValue) {
-    var dictionary = [];
-    var data = {
-      val: getNextValue(0),
-      position: resetValue,
-      index: 1
-    };
-    var result = [];
-    var enlargeIn = 4;
-    var dictSize = 4;
-    var numBits = 3;
-    var entry = '';
-    var w;
-    var c;
-
-    for (var i = 0; i < 3; i++) {
-      dictionary[i] = i;
-    }
-
-    switch (_commonRep3(data, Math.pow(2, 2), resetValue, getNextValue)) {
-      case 0:
-        c = f(_commonRep3(data, Math.pow(2, 8), resetValue, getNextValue));
-        break;
-
-      case 1:
-        c = f(_commonRep3(data, Math.pow(2, 16), resetValue, getNextValue));
-        break;
-
-      case 2:
-        return '';
-    }
-
-    dictionary[3] = c;
-    w = c;
-    result.push(c); // eslint-disable-next-line
-
-    while (true) {
-      if (data.index > length) {
-        return '';
-      }
-
-      switch (c = _commonRep3(data, Math.pow(2, numBits), resetValue, getNextValue)) {
-        case 0:
-          dictionary[dictSize++] = f(_commonRep3(data, Math.pow(2, 8), resetValue, getNextValue));
-          c = dictSize - 1;
-          enlargeIn--;
-          break;
-
-        case 1:
-          dictionary[dictSize++] = f(_commonRep3(data, Math.pow(2, 16), resetValue, getNextValue));
-          c = dictSize - 1;
-          enlargeIn--;
-          break;
-
-        case 2:
-          return result.join('');
-      }
-
-      if (enlargeIn === 0) {
-        enlargeIn = Math.pow(2, numBits);
-        numBits++;
-      }
-
-      if (dictionary[c]) {
-        entry = dictionary[c];
-      } else {
-        if (c === dictSize) {
-          entry = w + w.charAt(0);
-        } else {
-          return null;
-        }
-      }
-
-      result.push(entry);
-      dictionary[dictSize++] = w + entry.charAt(0);
-      enlargeIn--;
-      w = entry;
-
-      if (enlargeIn === 0) {
-        enlargeIn = Math.pow(2, numBits);
-        numBits++;
-      }
-    }
-  }
-
-  function toUTF16(input) {
-    if (input == null) return '';
-    return compress(input, 15, function (a) {
-      return f(a + 32);
-    }) + ' ';
-  }
-  function fromUTF16(compressed) {
-    if (compressed == null) return '';
-    if (compressed === '') return null;
-    return decompress(compressed.length, 16384, function (index) {
-      return compressed.charCodeAt(index) - 32;
-    });
-  }
-
-  /**
-   * Sets user cookie
-   * @param {string} key name of cookie
-   * @param {string} value cookie value
-   * @param {string} exp cookie expiry
-   * @param {string} path url path
-   * @param {string} domain supported domain
-   * @param {boolean} isSecure Sets security flag
-   */
-
-  function setCookie(key, value) {
-    if (key && typeof value !== 'undefined') {
-      var domain = loc.hostname;
-      var isSecure = loc.protocol === 'https:';
-      var transformedValue = value;
-
-      if (isObject(value)) {
-        transformedValue = JSON.stringify(value);
-      }
-
-      var cookiePath = "; path=/";
-      var cookieDomain = LOCAL_ENV.indexOf(domain) === -1 ? "; domain=".concat(trim(domain)) : '';
-      var secureFlag = isSecure ? '; secure' : '';
-      document.cookie = "".concat(key, " = ").concat(transformedValue).concat(cookieDomain).concat(cookiePath).concat(secureFlag);
-    }
-  }
-  /**
-   * Get's cookie value
-   * @param {string} key Key
-   * @param {boolean} trimResult Flag to trim the value
-   */
-
-
-  function getCookie(key) {
-    if (key) {
-      var cookieStr = decodeURIComponent(document.cookie);
-      var value = '';
-      each(cookieStr.split(';'), function (cookiePair) {
-        var keyPart = "".concat(key, "=");
-        var indexOfKey = cookiePair.indexOf(keyPart);
-
-        if (indexOfKey > -1) {
-          value = trim(cookiePair.substring(indexOfKey + keyPart.length, cookiePair.length));
-          return false;
-        }
-      });
-      return value;
-    }
-
-    return '';
-  }
-  /**
-   * Returns true if local storage is accessible
-   */
-
-
-  function isStorageAvailable() {
-    try {
-      sessionStorage.setItem('testKey', 'testValue');
-      sessionStorage.removeItem('testKey');
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
-
-  function set$1(key, value) {
-    if (isStorageAvailable()) {
-      return sessionStorage.setItem(key, toUTF16(isObject(value) ? JSON.stringify(value) : value));
-    }
-
-    return setCookie(key, value);
-  }
-  function get(key) {
-    if (isStorageAvailable()) {
-      var value = fromUTF16(sessionStorage.getItem(key));
-
-      try {
-        value = JSON.parse(value);
-        return value;
-      } catch (e) {
-        return value;
-      }
-    }
-
-    return getCookie(key);
-  }
-
-  var store = {
-    set: function set() {
-      return set$1.apply(this, arguments);
-    },
-    get: function get$1() {
-      return get.apply(this, arguments);
-    }
-  };
-
-  var StorageLib = /*#__PURE__*/function () {
-    function StorageLib() {
-      _classCallCheck(this, StorageLib);
-    }
-
-    _createClass(StorageLib, [{
-      key: "getDataFromStore",
-      value: function getDataFromStore(path) {
-        var paths = assign(store.get('routeStore'));
-        return paths[path];
-      }
-    }, {
-      key: "setDataToStore",
-      value: function setDataToStore(path, data) {
-        var paths = assign(store.get('routeStore'));
-
-        if (paths[path]) {
-          if (!data || isPureObject(data) && Object.keys(data).length === 0) return false;
-        }
-
-        return store.set('routeStore', assign({}, paths, _defineProperty({}, path, data)));
-      }
-    }]);
-
-    return StorageLib;
-  }();
-
-  var libs = new StorageLib();
-
   var RouterEvent = function RouterEvent(routeInfo, currentEvent) {
     _classCallCheck(this, RouterEvent);
 
@@ -696,7 +296,7 @@
 
     var _routerInstance$confi = routerInstance.config,
         location = _routerInstance$confi.location,
-        preservePath = _routerInstance$confi.preservePath;
+        history = _routerInstance$confi.history;
     this.route = routeObject.path;
     this.hashRouting = routeObject.hash;
     this.routerInstance = routerInstance;
@@ -705,22 +305,9 @@
     this.path = trim(location.pathname);
     this.hash = location.hash;
     this.search = trim(location.search.substring(1));
-    this.hashSearch = trim(location.hash && location.hash.split('?')[1]); // Extract data
-
-    var path = routeObject.path;
-
-    if (this.hashRouting) {
-      path = "/#".concat(path);
-
-      if (preservePath) {
-        path = "".concat(location.pathname).concat(path.substring(1));
-      }
-    } // Set route data to store
-
-
-    libs.setDataToStore(path, this.originalEvent.state && this.originalEvent.state.data); // Get current data from store
-
-    this.data = libs.getDataFromStore(path);
+    this.hashSearch = trim(location.hash && location.hash.split('?')[1]);
+    var state = this.originalEvent.state;
+    this.data = state && state.data || history.state && history.state.data;
   };
 
   function collate() {
@@ -828,6 +415,8 @@
     }
     return false;
   };
+
+  var loc = window.location;
 
   /**
    * Checks if query parameter key is a complex notation
@@ -1026,7 +615,6 @@
     var _this$config = this.config,
         preservePath = _this$config.preservePath,
         hashRouting = _this$config.hashRouting,
-        location = _this$config.location,
         history = _this$config.history;
     var routeObject = assign({
       replace: replace,
@@ -1041,7 +629,7 @@
     var preserveQuery = routeObject.preserveQuery,
         data = routeObject.data,
         _routeObject$pageTitl = routeObject.pageTitle,
-        pageTitle = _routeObject$pageTitl === void 0 ? document.querySelector('head title').textContent : _routeObject$pageTitl;
+        pageTitle = _routeObject$pageTitl === void 0 ? document.querySelector('head > title').textContent : _routeObject$pageTitl;
     var routeParts = routeStr.split('?'); // Check if query string is an object
 
     if (isObject(queryString)) {
@@ -1065,10 +653,8 @@
         if (preservePath) {
           routeStr = "".concat(routeStr.substring(1));
         }
-      } // Sync data to store before appending query string. Query string should have no effect on stored data
+      } // Append query string
 
-
-      libs.setDataToStore(preservePath ? "".concat(location.pathname).concat(routeStr) : routeStr, data); // Append query string
 
       routeStr = "".concat(routeStr).concat(queryString ? "?".concat(queryString) : '');
       history[replace ? 'replaceState' : 'pushState']({
