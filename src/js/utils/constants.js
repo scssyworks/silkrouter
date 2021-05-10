6cb97c88c3513a65946884805101858962779bb4
Sachin Singh <ssingh259@sapient.com>
2021-05-11 05:01:24 +0530

    Removed unused constants

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -1,7 +1,6 @@
 /**
  * Router constants
  */
-export const HASH_CHANGE = 'hashchange';
 export const POP_STATE = 'popstate';
 export const REG_ROUTE_PARAMS = /:[^/]+/g;
 export const REG_PATHNAME = /^\/(?=[^?]*)/;
@@ -9,16 +8,16 @@ export const REG_COMPLEX = /\[/;
 export const REG_VARIABLE = /([^[]+)\[([^[]*)\]/;
 export const REG_REPLACE_BRACKETS = /\[([^[]*)\]/;
 export const REG_REPLACE_NEXTPROP = /[^[]+/;
-export const HISTORY_UNSUPPORTED = 'Current browser does not support history object';
+export const HISTORY_UNSUPPORTED =
+  'Current browser does not support history object';
 export const INVALID_ROUTE = 'Route string is not a pure route';
-export const LOCAL_ENV = ['localhost', '0.0.0.0', '127.0.0.1', null];
 export const VIRTUAL_PUSHSTATE = 'vpushstate';
 export const CACHED_FIELDS = [
-    'route',
-    'hashRouting',
-    'path',
-    'hash',
-    'search',
-    'hashSearch',
-    'data'
+  'route',
+  'hashRouting',
+  'path',
+  'hash',
+  'search',
+  'hashSearch',
+  'data',
 ];
