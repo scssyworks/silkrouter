e3c908cfdb755bfba85f345a2b7d939797dde5d3
Sachin Singh <contactsachinsingh@gmail.com>
2026-07-19 22:57:14 +0530

    CI fixes

---
+++ scripts/release.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import { execSync } from 'node:child_process';
+import fs from 'node:fs';
+
+const baseVersion = process.argv[2];
+
+const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+const packageName = packageJson.name;
+
+let versions = [];
+
+try {
+  const output = execSync(`npm view ${packageName} versions --json`, {
+    encoding: 'utf-8',
+  });
+  versions = JSON.parse(output);
+} catch {
+  versions = [];
+}
+
+const rcVersions = versions
+  .filter(v => v.startsWith(`${baseVersion}-rc.`))
+  .map(v => Number(v.split('-rc.')[1]))
+  .filter(Number.isFinite);
+
+const nextRc = rcVersions.length > 0 ? Math.max(...rcVersions) + 1 : 1;
+
+process.stdout.write(String(nextRc));
